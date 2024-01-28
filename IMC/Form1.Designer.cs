0de6813221d462d230fe4b268a70da6740e51530
jj-developers <jjdevelopersmtz@gmail.com>
2024-01-28 15:28:03 -0600

    Agregar archivos de proyecto.

---
+++ IMC/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_talla = new System.Windows.Forms.TextBox();
+            this.txt_imc = new System.Windows.Forms.TextBox();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.diagnostico = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_talla
+            // 
+            this.txt_talla.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_talla.Location = new System.Drawing.Point(139, 134);
+            this.txt_talla.Name = "txt_talla";
+            this.txt_talla.Size = new System.Drawing.Size(100, 40);
+            this.txt_talla.TabIndex = 0;
+            this.txt_talla.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_talla_KeyPress);
+            // 
+            // txt_imc
+            // 
+            this.txt_imc.Enabled = false;
+            this.txt_imc.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_imc.Location = new System.Drawing.Point(332, 134);
+            this.txt_imc.Name = "txt_imc";
+            this.txt_imc.Size = new System.Drawing.Size(100, 40);
+            this.txt_imc.TabIndex = 1;
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_peso.Location = new System.Drawing.Point(139, 195);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 40);
+            this.txt_peso.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Altura (Metros)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(300, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "IMC";
+            // 
+            // diagnostico
+            // 
+            this.diagnostico.AutoSize = true;
+            this.diagnostico.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diagnostico.Location = new System.Drawing.Point(59, 307);
+            this.diagnostico.Name = "diagnostico";
+            this.diagnostico.Size = new System.Drawing.Size(0, 25);
+            this.diagnostico.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(323, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 47);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(185, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 47);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Otro";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(47, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 47);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Aceptar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.diagnostico);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.txt_imc);
+            this.Controls.Add(this.txt_talla);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_talla;
+        private System.Windows.Forms.TextBox txt_imc;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label diagnostico;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ IMC/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float peso, altura;
+            float imc = 0;
+
+            // Suponiendo que txt_peso y txt_talla son controles TextBox donde los usuarios ingresan peso y altura en metros
+            if (float.TryParse(txt_peso.Text, out peso) && float.TryParse(txt_talla.Text, out altura))
+            {
+                imc = peso / (altura * altura);
+                imc = (float)Math.Round(imc, 2); // Redondea el IMC a dos decimales
+                txt_imc.Text = imc.ToString();
+                diagnostico.Text = obtenerDiagnostico(imc);
+            }
+
+
+        }
+
+        private string obtenerDiagnostico(float imc)
+        {
+            string diagnostico;
+
+            switch (imc)
+            {
+                case float _ when imc < 18.5:
+                    diagnostico = "Bajo peso";
+                    break;
+                case float _ when imc < 24.9:
+                    diagnostico = "Peso normal";
+                    break;
+                case float _ when imc < 29.9:
+                    diagnostico = "Sobrepeso";
+                    break;
+                case float _ when imc < 34.9:
+                    diagnostico = "Obesidad tipo I (Moderada)";
+                    break;
+                case float _ when imc < 39.9:
+                    diagnostico = "Obesidad tipo II (Severa)";
+                    break;
+                default:
+                    diagnostico = "Obesidad tipo III (Muy severa o mórbida)";
+                    break;
+            }
+
+            return diagnostico;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_imc.Text = "";
+            txt_peso.Text = "";
+            txt_talla.Text = "";
+            diagnostico.Text = "";
+        }
+
+        private void txt_talla_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
